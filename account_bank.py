33df5d6b49023f5cdee85486c8b84c88e4ef0a41
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-12-03 08:07:14 +0530

    2

--- account_bank.py
+++ account_bank.py
@@ -1,12 +1,42 @@
 
-class Account:
-    def __init__(self, name, initial_amount):
-       self.name = name
-       self.balance = initial_amount
+class Saving_Account:
+    def __init__(self,holder_name,initial_balance):
+        self.holder_name=holder_name
+        self.balance=initial_balance
+        print(f"Saving Account Created is {self.holder_name} with balance {self.balance:.2f}")
+
+    def get_balance(self):
+        return print(f"Current balance of {self.holder_name} is {self.balance:.2f}")
+
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+        print(f"Deposited {amount:.2f} into {self.holder_name} account balance is {self.balance:.2f}")
     
-
-    def get_Balance(self):
-       return self.balance
-
-    def deposit(self, amount):
-       self.balance += amount
+    def check_balance(self,amount):
+        if self.balance>=amount:
+            return print(f"You have enough balance to withdraw {amount:.2f}")
+        else:
+            return print(f"You do not have enough balance to withdraw {amount:.2f}")
+        
+        
+    
+    def withdraw(self,amount):
+        try:
+            self.check_balance(amount)
+            self.balance=self.balance-amount
+            print(f"Withdrawn {amount:.2f} from {self.holder_name} account balance is {self.balance:.2f}")
+        except  ValueError as error :
+            print("Insufficient Balance")
+            return error
+    
+    def transfer(self,amount,receiver_name):
+        try:
+            self.check_balance(amount)
+            self.balance=self.balance-amount
+            print(f"Transferred {amount:.2f} from {self.holder_name} to {receiver_name} account balance is {self.balance:.2f}")
+        except :
+            print("Insufficient Balance")
+            
+   
+       
+        
---
+++ project_oop.py
@@ -0,0 +1,4 @@
+from account_bank import *
+Siva=Saving_Account("Siva",1000)
+print(Siva)
+Siva.get_balance()
