03563d24cf1e24c11bcb7a4365a2996615f82a43
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-12-03 08:30:16 +0530

    3

--- account_bank.py
+++ account_bank.py
@@ -1,3 +1,7 @@
+class BlanceException(Exception):
+    pass
+
+
 
 class Saving_Account:
     def __init__(self,holder_name,initial_balance):
@@ -14,28 +18,32 @@ class Saving_Account:
     
     def check_balance(self,amount):
         if self.balance>=amount:
-            return print(f"You have enough balance to withdraw {amount:.2f}")
+            return 
         else:
-            return print(f"You do not have enough balance to withdraw {amount:.2f}")
+            raise BlanceException(f"You do not have enough balance to withdraw {amount:.2f}")
         
         
-    
     def withdraw(self,amount):
         try:
             self.check_balance(amount)
             self.balance=self.balance-amount
             print(f"Withdrawn {amount:.2f} from {self.holder_name} account balance is {self.balance:.2f}")
-        except  ValueError as error :
-            print("Insufficient Balance")
-            return error
+        except  BlanceException as error :
+            return print(f"Insufficient Balance {error}")
     
     def transfer(self,amount,receiver_name):
         try:
             self.check_balance(amount)
-            self.balance=self.balance-amount
+            self.withdraw(amount)
+            receiver_name.deposit(amount)
             print(f"Transferred {amount:.2f} from {self.holder_name} to {receiver_name} account balance is {self.balance:.2f}")
-        except :
-            print("Insufficient Balance")
+        except  BlanceException as error :
+            return print(f"Insufficient Balance {error}")
+        
+class Normal_Account(Saving_Account):   # Inheritance
+    def get_balance(self):
+        return print(f"Current balance of {self.holder_name} is {self.balance + self.balance*0.05:.2f}")
+            
             
    
        
--- project_oop.py
+++ project_oop.py
@@ -1,4 +1,21 @@
 from account_bank import *
 Siva=Saving_Account("Siva",1000)
-print(Siva)
+# print(Siva)
+# Siva.get_balance()
+
+danu=Saving_Account("Danu",1000)
+
+Siva.transfer(500,danu)
+# danu.get_balance()
+# Siva.get_balance()
+danu.get_balance()
+
+
+suji=Normal_Account("Suji",1000)
+suji.get_balance()
+
+print("----------------------------------------------------------------------------------------")
+
+suji.transfer(500,Siva)
 Siva.get_balance()
+suji.get_balance()
